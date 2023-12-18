53b7fdd5a76b0b726010018d3d0146d0413cefb8
RosieOh <20172207@gm.hannam.ac.kr>
2023-12-19 00:00:17 +0900

    🥷Build : 각 리소스별 경로 설정 파일 완🥷

---
+++ src/main/java/com/vviewnego/config/ServletConfig.java
@@ -0,0 +1,33 @@
+package com.vviewnego.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class ServletConfig implements WebMvcConfigurer {
+
+    @Value("${resource.path}")
+    private String resourcePath;
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/");
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/static/assets/");
+        registry.addResourceHandler("/clEditor/**").addResourceLocations("classpath:/static/clEditor/");
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+
+        registry.addResourceHandler(uploadPath).addResourceLocations(resourcePath);
+
+    }
+
+}
