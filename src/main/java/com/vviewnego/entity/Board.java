9d9f51260e588e13c71d64eb332df2c51101ea36
RosieOh <20172207@gm.hannam.ac.kr>
2023-12-19 00:14:02 +0900

    🥷feat : Board에 필요한 DTO, Entity 작성 완🥷

--- src/main/java/com/vviewnego/entity/Board.java
+++ src/main/java/com/vviewnego/entity/Board.java
@@ -2,44 +2,51 @@ package com.vviewnego.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
-@ToString
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
-@EntityListeners(value = { AuditingEntityListener.class })
-public class Board {
+@NoArgsConstructor
+@ToString(exclude = "imageSet")
+public class Board extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer bno;
+    private Long bno;
 
-    @Column(length = 200, nullable = false)
+    @Column(length = 500, nullable = false)
     private String title;
 
     @Column(length = 2000, nullable = false)
     private String content;
 
-    @Column(length = 50, nullable = false)
+    private String boardType;
+
     private String writer;
 
-    @CreationTimestamp  //@CreatedDate = LocalDate
-    @Column(name = "regdate", updatable = false)
-    private LocalDateTime regDate;
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    @BatchSize(size = 20)
+    private Set<BoardImage> imageSet = new HashSet<>();
 
-    @UpdateTimestamp    //@LastModifiedDate = LocalDate
-    @Column(name = "moddate")
-    private LocalDateTime modDate;
 
-    public void change(String title, String content){
+    public void create(String title, String content,String writer) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+    }
+
+    public void change(String title, String content) {
         this.title = title;
         this.content = content;
     }
